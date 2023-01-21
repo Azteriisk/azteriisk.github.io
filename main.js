be24c8f5c96cd7251916fa0738397e10fd1189d3
Asterisk <alec@asterisk.best>
2023-01-21 14:44:46 -0600

    you wouldn't get it dad

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-var donateButton = document.getElementById("donate");
 var unDonateButton = document.getElementById("undonate");
 var tracker = document.getElementById("container");
 let i = 0;
 
 donate = function(){
+    var donateButton = document.getElementById("donate");
     donateButton.style.color = 'red'
     tracker.innerHTML = '2';
 }
