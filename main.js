93143f2100c798f78c8a3ec64430573967f61c40
Asterisk <alec@asterisk.best>
2023-01-29 15:55:29 -0600

    jk now it works xoxo

--- main.js
+++ main.js
@@ -7,14 +7,13 @@ tracker.innerHTML = i;
 
 donate = function(){
     donateButton.style.backgroundColor = 'red';
-    tracker.innerHTML = i++;
+    tracker.innerHTML = ++i;
 }
 
 undonate = function(){
     unDonateButton.style.backgroundColor = 'blue';
-    tracker.innerHTML = i--;
+    tracker.innerHTML = --i;
 }
 
 donateButton.onclick = donate;
-
 unDonateButton.onclick = undonate;
