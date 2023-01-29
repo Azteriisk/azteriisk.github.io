3ce840676798e317be3425395cb08b2c960f46ef
Asterisk <alec@asterisk.best>
2023-01-29 17:28:34 -0600

    reset colors by clicking on the tracker container

--- main.js
+++ main.js
@@ -6,14 +6,28 @@ var i = 0;
 tracker.innerHTML = i;
 
 donate = function(){
+    unDonateButton.style.backgroundColor = '';
+    unDonateButton.style.color = '';
     donateButton.style.backgroundColor = 'red';
+    donateButton.style.color = 'white';
     tracker.innerHTML = ++i;
 }
 
 undonate = function(){
+    donateButton.style.backgroundColor = '';
+    donateButton.style.color = '';
     unDonateButton.style.backgroundColor = 'blue';
+    unDonateButton.style.color = 'white';
     tracker.innerHTML = --i;
+}
+
+reset = function() {
+    donateButton.style.backgroundColor = '';
+    donateButton.style.color = '';
+    unDonateButton.style.backgroundColor = '';
+    unDonateButton.style.color = '';
 }
 
 donateButton.onclick = donate;
 unDonateButton.onclick = undonate;
+tracker.onclick = reset;
